9950bb3da11de3beceac5d2a334ac952765eaa14
Udd Robert <robert.udd@lm.se>
2019-12-04 10:36:08 +0100

    Fewer int conversions

--- day4/part1.py
+++ day4/part1.py
@@ -1,15 +1,15 @@
 no = "240920"
 count = 0
-while int(no) <= 789857:
+while no <= "789857":
     double = False
     for i in range(5):
-        if int(no[i]) > int(no[i+1]):
+        if no[i] > no[i+1]:
             #no = no[:i+1] + no[i] + no[i+2:]
             no = str(int(no) + 1)
             break
         if no[i] == no[i+1]:
             double = True
-    else: 
+    else:
         if double:
             count += 1
         no = str(int(no) + 1)
--- day4/part2.py
+++ day4/part2.py
@@ -1,9 +1,9 @@
 no = "240920"
 count = 0
-while int(no) <= 789857:
+while no <= "789857":
     double = 0
     for i in range(5):
-        if int(no[i]) > int(no[i+1]):
+        if no[i] > no[i+1]:
             #no = no[:i+1] + no[i] + no[i+2:]
             no = str(int(no) + 1)
             break
@@ -15,8 +15,8 @@ while int(no) <= 789857:
             elif i == 4 and no[i-1] != no[i]:
                 double += 1
 
-            
-    else: 
+
+    else:
         if double >= 1:
             count += 1
         no = str(int(no) + 1)
