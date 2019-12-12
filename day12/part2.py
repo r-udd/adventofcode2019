c294b4afaf974db4290c1acfe98406b742c66c09
Robert Udd <robert.udd@hotmail.com>
2019-12-12 18:03:59 +0100

    Done part 2

--- day12/part2.py
+++ day12/part2.py
@@ -1,56 +1,79 @@
 import itertools
+import fractions
 
-pos = []
-vel = []
+def lcm(a, b):
+    return abs(a*b) // fractions.gcd(a, b)
+
+def get_vel(m1, m2, v1, v2):
+    if m1 < m2:
+        return v1+1, v2-1
+    elif m2 < m1:
+        return v1-1, v2+1
+    return v1, v2
+
+x = []
+y = []
+z = []
 prevenergy = 0
 with open('input.txt') as f:
     for line in f:
         inp = line.split('=')
-        x = int(inp[1].split(',')[0])
-        y = int(inp[2].split(',')[0])
-        z = int(inp[3].split('>')[0])
-        pos.append([x, y, z])
-        vel.append([0,0,0])
+        xpos = int(inp[1].split(',')[0])
+        ypos = int(inp[2].split(',')[0])
+        zpos = int(inp[3].split('>')[0])
+        x.append(xpos)
+        y.append(ypos)
+        z.append(zpos)
+x += [0,0,0,0]#+4 for velocity
+y += [0,0,0,0]#+4 for velocity
+z += [0,0,0,0]#+4 for velocity
 
 pairs = itertools.combinations([0,1,2,3],2)
 p = list(pairs)
 inp = 5
-prevstates=set()
+prevstatesx={}
+prevstatesy={}
+prevstatesz={}
+found = [0, 0, 0]
 count = 0
 while True:
-    #state = tuple((p[i] for p in pos for i in range(3)))
-    state = tuple((v[i] for v in vel for i in range(3)))
-    
-    energy = 0
-    for moon in range(4):
-        kin = 0
-        pot = 0
-        for i in range(3):
-            pot += abs(pos[moon][i])
-            kin += abs(vel[moon][i])
-        energy += pot * kin
+    xstate = tuple(x)
+    ystate = tuple(y)
+    zstate = tuple(z)
+    if not found[0] and xstate in prevstatesx:
+        print('FOUND PREV STATE X', count, 'prev count', prevstatesx[xstate])
+        found[0] = count
+    if not found[1] and ystate in prevstatesy:
+        print('FOUND PREV STATE Y', count, 'prev count', prevstatesy[ystate])
+        found[1] = count
+    if not found[2] and zstate in prevstatesz:
+        print('FOUND PREV STATE Z', count, 'prev count', prevstatesz[zstate])
+        found[2] = count
+    if all(found):
+        break
 
-    if state in prevstates:
-        print(count)
-    prevstates.add(state)
+    prevstatesx[xstate] = count
+    prevstatesy[ystate] = count
+    prevstatesz[zstate] = count
     for pair in p:
         moon1 = pair[0]
         moon2 = pair[1]
-        for i in range(3):
-            if pos[moon1][i] < pos[moon2][i]:
-                vel[moon1][i] += 1
-                vel[moon2][i] -= 1
-            elif pos[moon2][i] < pos[moon1][i]:
-                vel[moon1][i] -= 1
-                vel[moon2][i] += 1
+        x[moon1+4], x[moon2+4] = get_vel(x[moon1], x[moon2], x[moon1+4], x[moon2+4])
+        y[moon1+4], y[moon2+4] = get_vel(y[moon1], y[moon2], y[moon1+4], y[moon2+4])
+        z[moon1+4], z[moon2+4] = get_vel(z[moon1], z[moon2], z[moon1+4], z[moon2+4])
     for moon in range(4):
-        for i in range(3):
-            pos[moon][i] += vel[moon][i]
+        x[moon] += x[moon+4]
+        y[moon] += y[moon+4]
+        z[moon] += z[moon+4]
     count += 1
     
-    if count % 1000 == 0:
-        #print(energy, energy - prevenergy)
-        prevenergy = energy
+    if count % 10000 == 0:
+        print(count, x)
+    # if count == 100:
+    #     break
 
-print(count)
+print('x', x)
+print('y', y)
+print('z', z)
+print(lcm(lcm(found[0], found[1]), found[2]))
 #Only input once
