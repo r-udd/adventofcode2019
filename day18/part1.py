3d0b846f2abf643bd78b45f2276726d1e1fdd6c7
Udd Robert <robert.udd@lm.se>
2019-12-18 15:56:11 +0100

    WIP part 1

--- day18/part1.py
+++ day18/part1.py
@@ -2,6 +2,8 @@ from collections import defaultdict
 from os import system, name
 
 # define our clear function
+
+
 def clear():
     # for windows
     if name == 'nt':
@@ -10,10 +12,11 @@ def clear():
     else:
         _ = system('clear')
 
+
 def printcave(cave, player, keys, doors, maxx, maxy):
     for y in range(maxy+1):
         for x in range(maxx+1):
-            pos = (x,y)
+            pos = (x, y)
             if pos == player:
                 print('@', end='')
             elif pos in keys:
@@ -21,70 +24,102 @@ def printcave(cave, player, keys, doors, maxx, maxy):
             elif pos in doors:
                 print(doors[pos], end='')
             else:
-                print(cave[(x,y)], end='')
+                print(cave[(x, y)], end='')
         print()
 
-def findnextkey(cave, player, keys, doors):
-    todo = [(player, 0)]
+
+def findnextkey(cave, p, minsteps):
+    todo = [(p['player'], 0)]
     visited = set()
-    dirs = [(0,-1), (0,1), (-1,0), (1,0)]
+    dirs = [(0, -1), (0, 1), (-1, 0), (1, 0)]
     possiblekeys = []
     while todo:
         pos, steps = todo.pop(0)
+        if steps > minsteps:
+            continue
         visited.add(pos)
         for d in dirs:
             newpos = (pos[0] + d[0], pos[1] + d[1])
             if cave[newpos] == '#':
                 continue
-            elif newpos in doors:
+            elif newpos in p['doors']:
                 continue
             elif newpos in visited:
                 continue
-            elif newpos in keys:
+            elif newpos in p['keys']:
                 possiblekeys.append((newpos, steps + 1))
             elif cave[newpos] == '.':
                 todo.append((newpos, steps + 1))
     return possiblekeys
 
 
-cave = defaultdict(lambda : '#')
+cave = defaultdict(lambda: '#')
 keys = {}
 doors = {}
 player = ()
-with open('test2.txt') as f:
+with open('test4.txt') as f:
     maxx = 0
     maxy = 0
     for y, l in enumerate(f):
         for x, char in enumerate(l.rstrip()):
             maxx = max(x, maxx)
             if char == '#':
-                cave[(x,y)] = '#'
+                cave[(x, y)] = '#'
                 continue
             elif char == '@':
-                player = (x,y)
+                player = (x, y)
             elif char.isupper():
-                doors[(x,y)] = char
+                doors[(x, y)] = char
             elif char.islower():
-                keys[(x,y)] = char
-            cave[(x,y)] = '.'
-        maxy = max(y, maxy)            
+                keys[(x, y)] = char
+            cave[(x, y)] = '.'
+        maxy = max(y, maxy)
 
 printcave(cave, player, keys, doors, maxx, maxy)
 
-steps = 0
-while keys:
-    possiblekeys = findnextkey(cave, player, keys, doors)
-    if len(possiblekeys) == 1:
-        steps += possiblekeys[0][1]
-        player = possiblekeys[0][0]
-        key = keys.pop(player)
-        for pos, val in doors.items():
-            if val.lower() == key:
-                doors.pop(pos)
-                break
-    else:
-        print('PRINT')
-        printcave(cave, player, keys, doors, maxx,maxy)
-        pass
-        
-print(steps)
+minsteps = 100000
+possibilities = [{'player': player, 'keys': keys, 'doors': doors, 'steps': 0}]
+possindex = 0
+while possindex < len(possibilities):
+    print('possindex', possindex, possibilities[possindex]['steps'])
+
+    # printcave(cave, possibilities[possindex]['player'],
+    #           possibilities[possindex]['keys'], possibilities[possindex]['doors'], maxx, maxy)
+
+    possiblekeys = findnextkey(
+        cave, possibilities[possindex], minsteps - possibilities[possindex]['steps'])
+    if len(possiblekeys) == 0:
+        possindex += 1
+        continue
+    if len(possiblekeys) != 1:
+        for newi in range(1, len(possiblekeys)):
+            newposs = {'player': possiblekeys[newi][0]}
+            newposs['steps'] = possibilities[possindex]['steps'] + \
+                possiblekeys[newi][1]
+            newposs['keys'] = possibilities[possindex]['keys'].copy()
+            key = newposs['keys'].pop(possiblekeys[newi][0])
+
+            doors = possibilities[possindex]['doors'].copy()
+            for pos, val in doors.items():
+                if val.lower() == key:
+                    doors.pop(pos)
+                    break
+            newposs['doors'] = doors
+            possibilities.append(newposs)
+
+    possibilities[possindex]['steps'] += possiblekeys[0][1]
+    player = possiblekeys[0][0]
+    possibilities[possindex]['player'] = player
+    key = possibilities[possindex]['keys'].pop(player)
+    # print('key', key)
+    for pos, val in possibilities[possindex]['doors'].items():
+        if val.lower() == key:
+            possibilities[possindex]['doors'].pop(pos)
+            break
+    if len(possibilities[possindex]['keys']) == 0:
+        # No keys left, done.
+        minsteps = min(minsteps, possibilities[possindex]['steps'])
+        possindex += 1
+        print('minsteps', minsteps)
+
+print(minsteps)
