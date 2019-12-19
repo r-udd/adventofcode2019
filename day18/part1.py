41eacd38385148de47cd4d84b96053f65292cfa5
Robert Udd <robert.udd@hotmail.com>
2019-12-19 06:44:08 +0100

    Precalc distances

--- day18/part1.py
+++ day18/part1.py
@@ -1,9 +1,8 @@
 from collections import defaultdict
 from os import system, name
+from heapq import *
 
 # define our clear function
-
-
 def clear():
     # for windows
     if name == 'nt':
@@ -27,30 +26,45 @@ def printcave(cave, player, keys, doors, maxx, maxy):
                 print(cave[(x, y)], end='')
         print()
 
+precomp = {}
 
-def findnextkey(cave, p, minsteps):
-    todo = [(p['player'], 0)]
+def calcdist(cave, start, target, doors):
+    todo = [(start, 0, [])]
     visited = set()
     dirs = [(0, -1), (0, 1), (-1, 0), (1, 0)]
-    possiblekeys = []
     while todo:
-        pos, steps = todo.pop(0)
-        if steps > minsteps:
-            continue
+        pos, steps, keysneeded = todo.pop(0)
         visited.add(pos)
         for d in dirs:
             newpos = (pos[0] + d[0], pos[1] + d[1])
             if cave[newpos] == '#':
                 continue
-            elif newpos in p['doors']:
-                continue
             elif newpos in visited:
                 continue
-            elif newpos in p['keys']:
-                possiblekeys.append((newpos, steps + 1))
+            elif newpos in doors:
+                keysneeded.append(doors[newpos].lower())
+                todo.append((newpos, steps + 1, keysneeded.copy()))
+            elif newpos == target:
+                return steps + 1, keysneeded
             elif cave[newpos] == '.':
-                todo.append((newpos, steps + 1))
-    return possiblekeys
+                todo.append((newpos, steps + 1, keysneeded.copy()))
+    print('WHY')
+
+def getallpossibilities(mem, start, keys, currkeys, step, minstep):
+    possible = {}
+    for pos, letter in keys.items():
+        if letter not in currkeys:
+            neededstep, neededkeys = mem[(start, pos)]
+            for nk in neededkeys:
+                if nk not in currkeys:
+                    break
+            else:
+                if neededstep + step < minstep:
+                    possible[pos] = letter
+                # else:
+                #     print('Too large')
+    return possible
+    
 
 
 cave = defaultdict(lambda: '#')
@@ -76,54 +90,42 @@ with open('input.txt') as f:
         maxy = max(y, maxy)
 
 printcave(cave, player, keys, doors, maxx, maxy)
+precalc = {}
+mem = {}
+for pos in keys.keys():
+    steps, keysneeded = calcdist(cave, player, pos, doors)
+    mem[(player,pos)] = [steps, keysneeded]
+    mem[(player,pos)] = [steps, keysneeded]
 
-minsteps = 100000
-possibilities = [{'player': player, 'keys': keys, 'doors': doors, 'steps': 0}]
-possindex = 0
-while possindex < len(possibilities):
-    #print('possindex', possindex, possibilities[possindex]['steps'])
+keylist = list(keys.keys())
+for i in range(len(keylist)):
+    for j in range(i+1, len(keylist)):
+        steps, keysneeded = calcdist(cave, keylist[i], keylist[j], doors)
+        mem[(keylist[i], keylist[j])] = [steps, keysneeded]
+        mem[(keylist[j], keylist[i])] = [steps, keysneeded]
 
-    # printcave(cave, possibilities[possindex]['player'],
-    #           possibilities[possindex]['keys'], possibilities[possindex]['doors'], maxx, maxy)
-    if possibilities[possindex]['steps'] > minsteps:
-        possindex += 1
+currkeys = set()
+workq = []
+heappush(workq, [[0, 0], 0, player, currkeys])
+minstep = 4200
+print('Start going through the possible paths')
+while workq:
+    _, step, start, currkeys = heappop(workq)
+    # print(len(currkeys), len(keys))
+    # input('STOP')
+    if step >= minstep:
         continue
-    possiblekeys = findnextkey(
-        cave, possibilities[possindex], minsteps - possibilities[possindex]['steps'])
-    if len(possiblekeys) == 0:
-        possindex += 1
-        continue
-    if len(possiblekeys) != 1:
-        for newi in range(1, len(possiblekeys)):
-            newposs = {'player': possiblekeys[newi][0]}
-            newposs['steps'] = possibilities[possindex]['steps'] + \
-                possiblekeys[newi][1]
-            newposs['keys'] = possibilities[possindex]['keys'].copy()
-            key = newposs['keys'].pop(possiblekeys[newi][0])
-
-            doors = possibilities[possindex]['doors'].copy()
-            for pos, val in doors.items():
-                if val.lower() == key:
-                    doors.pop(pos)
-                    break
-            newposs['doors'] = doors
-            possibilities.append(newposs)
-
-    possibilities[possindex]['steps'] += possiblekeys[0][1]
-    player = possiblekeys[0][0]
-    possibilities[possindex]['player'] = player
-    key = possibilities[possindex]['keys'].pop(player)
-    # print('key', key)
-    for pos, val in possibilities[possindex]['doors'].items():
-        if val.lower() == key:
-            possibilities[possindex]['doors'].pop(pos)
-            break
-    if len(possibilities[possindex]['keys']) == 0:
-        # No keys left, done.
-        prevmin = minsteps
-        minsteps = min(minsteps, possibilities[possindex]['steps'])
-        possindex += 1
-        if prevmin != minsteps:
-            print('minsteps', minsteps, 'index', possindex)
-
-print(minsteps)
+    if len(keys) == len(currkeys):
+        if step < minstep:
+            minstep = step
+            print('Antalet steg', step, 'remaining heap', len(workq))
+        else:
+            print('steg', step)
+    possibilities = getallpossibilities(mem, start, keys, currkeys, step, minstep)
+    for p,letter in possibilities.items():
+        c = currkeys.copy()
+        c.add(letter)
+        nextstep = step + mem[(start, p)][0]
+        if nextstep < minstep:
+            heappush(workq, [[-len(currkeys), nextstep], nextstep, p, c])
+print(minstep)
