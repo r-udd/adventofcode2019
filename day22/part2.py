16772e79001cd70019c284b278a873eb4067ed4f
r-udd <robert_udd@hotmail.com>
2019-12-23 00:19:24 +0100

    Update part2.py
    WIP part 2 day 22

--- day22/part2.py
+++ day22/part2.py
@@ -1,34 +1,65 @@
 instructions = []
 with open('input.txt') as f:
-    for line in f:
-        if line.startswith('deal into new stack'):
-            instructions.append('r')
-        elif line.startswith('deal with increment'):
-            instructions.append('di')
-            instructions.append(int(line.split()[-1]))
-        elif line.startswith('cut'):
-            instructions.append('c')
-            instructions.append(int(line.split()[-1]))
-        else:
-            print('Something is wrong')
+    lines = f.readlines()
+    lines.reverse()
 
-decksize = 119315717514047
-deck = [x for x in range(decksize)]
-while instructions:
-    i = instructions.pop(0)
-    if i == 'r':
-        deck.reverse()
-    elif i == 'c':
-        incr = instructions.pop(0)
-        deck = deck[incr:] + deck[:incr]
-    elif i == 'di':
-        incr = instructions.pop(0)
-        deck2 = deck.copy()
-        deck2.reverse()
-        for index in range(decksize):
-            deck[index * incr % decksize] = deck2.pop()
+#with open('inputreverse.txt', 'w') as f:
+#    f.writelines(lines)
+
+for line in lines:
+    if line.startswith('deal into new stack'):
+        instructions.append('r')
+    elif line.startswith('deal with increment'):
+        instructions.append('di')
+        instructions.append(int(line.split()[-1]))
+    elif line.startswith('cut'):
+        instructions.append('c')
+        instructions.append(int(line.split()[-1]))
     else:
         print('Something is wrong')
 
-print(decksize == len(deck))
-print(deck.index(2019))
+decksize = 119315717514047
+decksize = 10007
+index = 2020
+index = 7395
+
+states = []
+diffs = []
+diff = 0
+while True:
+    states.append(index)
+    if len(states) > 1:
+        diff = (index-states[-2]) % decksize
+        if diff in diffs:
+            break
+
+        diffs.append(diff)
+        #print(diff)
+    i = 0
+    offset
+    while i < len(instructions):
+        instruction = instructions[i]
+        i += 1
+        if instruction == 'r':
+            index3 = (-index - 1) % decksize
+            pass
+        elif instruction == 'c':
+            incr = instructions[i] % decksize
+            i += 1
+            index = (index + incr) % decksize
+        elif instruction == 'di':
+            incr = instructions[i]
+            i += 1
+            # x = index
+            y = pow(incr, -1, decksize)
+            index = (index * y) % decksize
+
+
+            # while x % incr != 0:
+            #     x += decksize
+            # index = x // incr
+            pass
+        else:
+            print('Something is wrong 2')
+    break
+print(index)
