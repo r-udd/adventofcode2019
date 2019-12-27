f67d6869cf02f6d69a0b4a99c6576a6fb2154f88
Robert Udd <robert.udd@hotmail.com>
2019-12-28 00:18:56 +0100

    Done part2. Last one this year!

--- day22/part2.py
+++ day22/part2.py
@@ -3,8 +3,23 @@ with open('input.txt') as f:
     lines = f.readlines()
     lines.reverse()
 
-#with open('inputreverse.txt', 'w') as f:
-#    f.writelines(lines)
+def revshuffle(instructions, index, decksize):
+    i = 0
+    while i < len(instructions):
+        instruction = instructions[i]
+        i += 1
+        if instruction == 'r':
+            index = (-index - 1) % decksize
+        elif instruction == 'c':
+            incr = instructions[i]
+            i += 1
+            index = (index + incr) % decksize
+        elif instruction == 'di':
+            incr = instructions[i]
+            i += 1
+            y = pow(incr, -1, decksize)
+            index = (index * y) % decksize
+    return index
 
 for line in lines:
     if line.startswith('deal into new stack'):
@@ -19,47 +34,12 @@ for line in lines:
         print('Something is wrong')
 
 decksize = 119315717514047
-decksize = 10007
-index = 2020
-index = 7395
+n = 101741582076661
+X = 2020
 
-states = []
-diffs = []
-diff = 0
-while True:
-    states.append(index)
-    if len(states) > 1:
-        diff = (index-states[-2]) % decksize
-        if diff in diffs:
-            break
-
-        diffs.append(diff)
-        #print(diff)
-    i = 0
-    offset
-    while i < len(instructions):
-        instruction = instructions[i]
-        i += 1
-        if instruction == 'r':
-            index3 = (-index - 1) % decksize
-            pass
-        elif instruction == 'c':
-            incr = instructions[i] % decksize
-            i += 1
-            index = (index + incr) % decksize
-        elif instruction == 'di':
-            incr = instructions[i]
-            i += 1
-            # x = index
-            y = pow(incr, -1, decksize)
-            index = (index * y) % decksize
-
-
-            # while x % incr != 0:
-            #     x += decksize
-            # index = x // incr
-            pass
-        else:
-            print('Something is wrong 2')
-    break
-print(index)
+Y = revshuffle(instructions, X, decksize)
+Z = revshuffle(instructions, Y, decksize)
+A = ((Y-Z) * pow(X-Y, -1, decksize)) % decksize
+B = (Y - A * X) % decksize
+#print(A, B)
+print((pow(A, n, decksize)*X + (pow(A, n, decksize)-1) * pow(A-1, -1, decksize) * B) % decksize)
